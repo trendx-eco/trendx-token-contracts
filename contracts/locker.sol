521ac2eb58fa00f70db991edb0d20845f7696852
vedaogithub <vedao.github@outlook.com>
2025-04-28 13:52:01 +0800

    recommit contracts

---
+++ contracts/locker.sol
@@ -0,0 +1,447 @@
+// Sources flattened with hardhat v2.22.17 https://hardhat.org
+
+// SPDX-License-Identifier: MIT
+
+// File @openzeppelin/contracts/token/ERC20/IERC20.sol@v4.9.0
+
+// Original license: SPDX_License_Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.9.0) (token/ERC20/IERC20.sol)
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+interface IERC20 {
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `to`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address to, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(
+        address owner,
+        address spender
+    ) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `from` to `to` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool);
+}
+
+// File @openzeppelin/contracts/utils/math/SafeMath.sol@v4.9.0
+
+// Original license: SPDX_License_Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.9.0) (utils/math/SafeMath.sol)
+
+// CAUTION
+// This version of SafeMath should only be used with Solidity 0.8 or later,
+// because it relies on the compiler's built in overflow checks.
+
+/**
+ * @dev Wrappers over Solidity's arithmetic operations.
+ *
+ * NOTE: `SafeMath` is generally not needed starting with Solidity 0.8, since the compiler
+ * now has built in overflow checking.
+ */
+library SafeMath {
+    /**
+     * @dev Returns the addition of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryAdd(
+        uint256 a,
+        uint256 b
+    ) internal pure returns (bool, uint256) {
+        unchecked {
+            uint256 c = a + b;
+            if (c < a) return (false, 0);
+            return (true, c);
+        }
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
+    function trySub(
+        uint256 a,
+        uint256 b
+    ) internal pure returns (bool, uint256) {
+        unchecked {
+            if (b > a) return (false, 0);
+            return (true, a - b);
+        }
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryMul(
+        uint256 a,
+        uint256 b
+    ) internal pure returns (bool, uint256) {
+        unchecked {
+            // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+            // benefit is lost if 'b' is also tested.
+            // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+            if (a == 0) return (true, 0);
+            uint256 c = a * b;
+            if (c / a != b) return (false, 0);
+            return (true, c);
+        }
+    }
+
+    /**
+     * @dev Returns the division of two unsigned integers, with a division by zero flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryDiv(
+        uint256 a,
+        uint256 b
+    ) internal pure returns (bool, uint256) {
+        unchecked {
+            if (b == 0) return (false, 0);
+            return (true, a / b);
+        }
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers, with a division by zero flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryMod(
+        uint256 a,
+        uint256 b
+    ) internal pure returns (bool, uint256) {
+        unchecked {
+            if (b == 0) return (false, 0);
+            return (true, a % b);
+        }
+    }
+
+    /**
+     * @dev Returns the addition of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `+` operator.
+     *
+     * Requirements:
+     *
+     * - Addition cannot overflow.
+     */
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a + b;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting on
+     * overflow (when the result is negative).
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     *
+     * - Subtraction cannot overflow.
+     */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a - b;
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `*` operator.
+     *
+     * Requirements:
+     *
+     * - Multiplication cannot overflow.
+     */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a * b;
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers, reverting on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator.
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a / b;
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * reverting when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a % b;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting with custom message on
+     * overflow (when the result is negative).
+     *
+     * CAUTION: This function is deprecated because it requires allocating memory for the error
+     * message unnecessarily. For custom revert reasons use {trySub}.
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     *
+     * - Subtraction cannot overflow.
+     */
+    function sub(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        unchecked {
+            require(b <= a, errorMessage);
+            return a - b;
+        }
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers, reverting with custom message on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function div(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        unchecked {
+            require(b > 0, errorMessage);
+            return a / b;
+        }
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * reverting with custom message when dividing by zero.
+     *
+     * CAUTION: This function is deprecated because it requires allocating memory for the error
+     * message unnecessarily. For custom revert reasons use {tryMod}.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function mod(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        unchecked {
+            require(b > 0, errorMessage);
+            return a % b;
+        }
+    }
+}
+
+// File contracts/XTTALocker.sol
+
+// Original license: SPDX_License_Identifier: MIT
+
+contract XTTALocker {
+    using SafeMath for uint256;
+
+    IERC20 public immutable token; // Locked Tokens
+    address public beneficiary; // Beneficiary address
+    uint256 public immutable totalAmount; // Total amount of locked funds
+    uint256 public immutable startTime; // The start time of lock-up
+    uint256 public constant RELEASE_INTERVAL = 1 days; // The time interval between each release (1 day)
+    uint256 public releasedAmount = 1428572000000000000000000; // Released quantity
+    uint256 public constant INITIAL_YEARLY_RELEASE = 200_000_000 * (10 ** 18); // Total release in the first year
+    uint256 public constant TOTAL_YEARS = 20; // 20 years in total
+    uint256 public constant DAYS_IN_A_YEAR = 360; // 360 days per year
+
+    event Released(uint256 amount);
+    event BeneficiaryUpdated(address newBeneficiary);
+
+    /**
+     * @dev Constructor
+     * @param _token The contract address of the locked tokens
+     * @param _beneficiary Beneficiary address
+     * @param _totalAmount Total locked amount
+     * @param _startTime Lock-up start time (TGE time)
+     */
+    constructor(
+        address _token,
+        address _beneficiary,
+        uint256 _totalAmount,
+        uint256 _startTime
+    ) {
+        require(_token != address(0), "Invalid token address");
+        require(_beneficiary != address(0), "Invalid beneficiary address");
+        require(_totalAmount > 0, "Total amount must be greater than 0");
+
+        token = IERC20(_token);
+        beneficiary = _beneficiary;
+        totalAmount = _totalAmount;
+        startTime = _startTime;
+    }
+
+    /**
+     * @dev Get the current number of tokens available
+     */
+    function releasableAmount() public view returns (uint256) {
+        if (block.timestamp < startTime) {
+            return 0; // Locking has not started
+        }
+
+        uint256 elapsedTime = block.timestamp.sub(startTime);
+        uint256 elapsedDays = elapsedTime / RELEASE_INTERVAL;
+        uint256 elapsedYears = elapsedDays / DAYS_IN_A_YEAR;
+
+        if (elapsedYears >= TOTAL_YEARS) {
+            elapsedYears = TOTAL_YEARS; //
+        }
+
+        uint256 totalReleasable = 0;
+        uint256 yearlyReleaseAmount = INITIAL_YEARLY_RELEASE;
+
+        for (uint256 i = 0; i < elapsedYears; i++) {
+            totalReleasable = totalReleasable.add(yearlyReleaseAmount);
+            yearlyReleaseAmount = yearlyReleaseAmount.div(2);
+        }
+
+        uint256 currentYearDays = elapsedDays % DAYS_IN_A_YEAR;
+        uint256 currentYearRelease = yearlyReleaseAmount
+            .mul(currentYearDays)
+            .div(DAYS_IN_A_YEAR);
+        totalReleasable = totalReleasable.add(currentYearRelease);
+
+        totalReleasable = totalReleasable > totalAmount
+            ? totalAmount
+            : totalReleasable;
+
+        return totalReleasable.sub(releasedAmount);
+    }
+    /**
+     * @dev Withdraw available tokens
+     */
+    function release() external {
+        require(
+            msg.sender == beneficiary,
+            "Only beneficiary can release tokens"
+        );
+        uint256 amount = releasableAmount();
+        require(amount > 0, "No tokens available for release");
+
+        releasedAmount = releasedAmount.add(amount);
+        token.transfer(beneficiary, amount);
+
+        emit Released(amount);
+    }
+
+    /**
+     * @dev Update beneficiary address
+     * @param _newBeneficiary New beneficiary address
+     */
+    function updateBeneficiary(address _newBeneficiary) external {
+        require(msg.sender == beneficiary, "Only beneficiary can update");
+        require(_newBeneficiary != address(0), "Invalid beneficiary address");
+
+        beneficiary = _newBeneficiary;
+        emit BeneficiaryUpdated(_newBeneficiary);
+    }
+}
